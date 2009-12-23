2f10727e8cafd0efd833a0494d2bb3ca59f3dcc8
John McMaster <JohnDMcMaster@gmail.com>
2009-12-22 22:07:12 -0500

    Misc copyright headers

--- uvudec/init/uvd_config.cpp
+++ uvudec/init/uvd_config.cpp
@@ -1,6 +1,8 @@
 /*
+UVNet Universal Decompiler (uvudec)
 Copyright 2008 John McMaster
 JohnDMcMaster@gmail.com
+Licensed under terms of the three clause BSD license, see LICENSE for details
 */
 
 #include "interpreter/uvd_python.h"
