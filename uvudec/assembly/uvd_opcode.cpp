e7f6cdafc3433e7de1a388d6dd948b4304cf6439
John McMaster <JohnDMcMaster@gmail.com>
2009-12-22 22:07:12 -0500

    Misc copyright headers

--- uvudec/assembly/uvd_opcode.cpp
+++ uvudec/assembly/uvd_opcode.cpp
@@ -1,3 +1,10 @@
+/*
+UVNet Universal Decompiler (uvudec)
+Copyright 2008 John McMaster
+JohnDMcMaster@gmail.com
+Licensed under terms of the three clause BSD license, see LICENSE for details
+*/
+
 #include <string.h>
 #include <string>
 #include <vector>
--- uvudec/assembly/uvd_register.cpp
+++ uvudec/assembly/uvd_register.cpp
@@ -1,3 +1,10 @@
+/*
+UVNet Universal Decompiler (uvudec)
+Copyright 2008 John McMaster
+JohnDMcMaster@gmail.com
+Licensed under terms of the three clause BSD license, see LICENSE for details
+*/
+
 #include "uvd_register.h"
 
 UVDRegisterShared::UVDRegisterShared()
