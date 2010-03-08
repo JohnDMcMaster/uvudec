baa2650e93db30053b2062cf37e2be24ea1e7124
John McMaster <JohnDMcMaster@gmail.com>
2010-03-08 15:30:49 -0500

    Unit testing

---
+++ uvudec/testing/main.cpp
@@ -0,0 +1,48 @@
+/*
+UVNet Universal Decompiler(uvudec)
+Copyright 2008 John McMaster <JohnDMcMaster@gmail.com>
+Licensed under terms of the three clause BSD license, see LICENSE for details
+
+Unit test
+*/
+
+//Based off of
+//http://www.evocomp.de/tutorials/tutorium_cppunit/howto_tutorial_cppunit_en.html
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+
+int main(int argc, char* argv[])
+{
+	printf("main: begin\n");
+
+	// informs test-listener about testresults
+	CPPUNIT_NS :: TestResult testresult;
+
+	// register listener for collecting the test-results
+	CPPUNIT_NS::TestResultCollector collectedresults;
+	testresult.addListener(&collectedresults);
+
+	// register listener for per-test progress output
+	CPPUNIT_NS::BriefTestProgressListener progress;
+	testresult.addListener(&progress);
+
+	// insert test-suite at test-runner by registry
+	CPPUNIT_NS::TestRunner testrunner;
+	testrunner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+	testrunner.run(testresult);
+
+	// output results in compiler-format
+	CPPUNIT_NS::CompilerOutputter compileroutputter(&collectedresults, std::cerr);
+	compileroutputter.write();
+
+	uint32_t wasSuccessful = collectedresults.wasSuccessful();
+
+	printf("main: done\n");
+
+	// return 0 if tests were successful
+	return wasSuccessful ? 0 : 1;
+}
