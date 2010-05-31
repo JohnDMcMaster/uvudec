2d2b3a1a6dd3572853c15d035eda8c23f02e7e68
John McMaster <JohnDMcMaster@gmail.com>
2010-05-31 16:14:49 -0700

    Mostly coprygith collaposing, but some small updates

--- uvudec/interpreter/uvd_javascript.h
+++ uvudec/interpreter/uvd_javascript.h
@@ -1,6 +1,14 @@
 /*
+UVNet Universal Decompiler (uvudec)
+Copyright 2008 John McMaster <JohnDMcMaster@gmail.com>
+Licensed under terms of the three clause BSD license, see LICENSE for details
+*/
+
+/*
 Several options availible for javascript engine
 */
+
+#include "config.h"
 
 #if defined(USING_SPIDERMONKEY)
 #include "uvd_javascript_spidermonkey.h"
@@ -9,6 +17,7 @@ Several options availible for javascript engine
 #include "uvd_javascript_spiderape.h"
 #define UVDJavascriptInterpreter UVDJavascriptSpiderapeInterpreter
 #else
+//Or maybe should just not include something
 #error Javascript support requires specifying an engine to use
 #endif
 
