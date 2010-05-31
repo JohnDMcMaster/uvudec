a367f85348ba48eae2d84867ecedc598c701a5a8
John McMaster <JohnDMcMaster@gmail.com>
2010-05-31 16:15:23 -0700

    Removed dead formatting code moved to UVDConfig

--- uvudec/language/uvd_format.cpp
+++ uvudec/language/uvd_format.cpp
@@ -3,43 +3,6 @@
 #include "uvd_config.h"
 #include "uvd_types.h"
 
-#if 0
-unsigned int g_addr_min = 0;
-//int g_addr_max = 0;
-unsigned int g_addr_max = 0;
-unsigned int g_hex_addr_print_width = 4;
-int g_addr_comment = false;
-int g_addr_label = false;
-int g_print_used = false;
-//FIXME: slow
-int g_called_sources = false;
-int g_called_count = true;
-//FIXME: this is relaly slow, disable by default
-int g_jumped_sources = false;
-int g_jumped_count = true;
-int g_print_string_table = false;
-int g_print_header = false;
-
-/* nothing (Intel), $ (MIPS) and % (gcc) are common */
-char reg_prefix[8] = "";
-
-//How many instances of the library are open
-//static unsigned int g_open_instances = 0;
-
-std::string g_mcu_name;
-std::string g_mcu_desc;
-std::string g_asm_imm_prefix;
-std::string g_asm_imm_prefix_hex;
-std::string g_asm_imm_postfix_hex;
-std::string g_asm_imm_suffix;
-int g_caps = FALSE;
-int g_binary = FALSE;
-struct uvd_mem_tag_t *g_memory_tags = NULL;
-int g_memoric = 0;
-int g_asm_instruction_info = 0;
-int g_print_block_id = TRUE;
-#endif
-
 UVDFormat::UVDFormat()
 {
 	m_compiler = NULL;
@@ -81,11 +44,6 @@ void printFormatting()
 	printf_debug("g_binary: %d\n", g_binary);
 	printf_debug("g_memoric: %d\n", g_memoric);
 	printf_debug("g_asm_instruction_info: %d\n", g_asm_instruction_info);
-
-	printf_debug("***\n");
-	printf_debug("Numeric:\n");
-	printf_debug("g_addr_min: 0x%.8X\n", g_addr_min);
-	printf_debug("g_addr_max: 0x%.8X\n", g_addr_max);
 
 	printf_debug("***\n");
 	printf_debug("Formatting strings:\n");
