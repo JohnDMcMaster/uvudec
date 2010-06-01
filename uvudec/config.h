21cd854ff9c5de5e7a7b858890c9fa59eb791749
John McMaster <JohnDMcMaster@gmail.com>
2010-05-31 17:04:25 -0700

    config.h placeholder

---
+++ uvudec/config.h
@@ -0,0 +1,10 @@
+/*
+UVNet Universal Decompiler (uvudec)
+Copyright 2008 John McMaster <JohnDMcMaster@gmail.com>
+Licensed under terms of the three clause BSD license, see LICENSE for details
+*/
+
+/*
+This file is a placeholder for automagically generated autoconf stuff in the future
+Currently these are defined in the Makefile rather than being defined here as part of ./configure process
+*/
