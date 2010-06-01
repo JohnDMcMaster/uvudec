a1915fcadcd0ae6625ed0cf898e892716f47a5cf
John McMaster <JohnDMcMaster@gmail.com>
2010-06-01 00:23:24 -0700

    Ranged analysis unit tests

---
+++ uvudec/testing/main_hook.h
@@ -0,0 +1,14 @@
+/*
+UVNet Universal Decompiler (uvudec)
+Copyright 2008 John McMaster <JohnDMcMaster@gmail.com>
+Licensed under terms of the three clause BSD license, see LICENSE for details
+*/
+
+#ifndef MAIN_HOOK_H
+#define MAIN_HOOK_H
+
+#include "uvd_types.h"
+
+uv_err_t uvudec_uvmain(int argc, char **argv);
+
+#endif
