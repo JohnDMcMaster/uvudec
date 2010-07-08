e37a83767f68d94c4f53610c619a285cea34f1e3
John McMaster <JohnDMcMaster@gmail.com>
2010-07-08 08:21:37 -0400

    Seperation of python interpreters, print function name change

---
+++ uvudec/interpreter/uvd_python_exec.cpp
@@ -0,0 +1,96 @@
+/*
+UVNet Universal Decompiler (uvudec)
+Copyright 2008 John McMaster <JohnDMcMaster@gmail.com>
+Licensed under terms of the three clause BSD license, see LICENSE for details
+*/
+
+#ifdef USING_PYTHON_EXEC
+
+#include <string>
+#include <vector>
+#include "uvd_util.h"
+#include "uvd_python.h"
+#include "uvd_config.h"
+
+UVDPythonExecInterpreter::UVDPythonExecInterpreter()
+{
+}
+
+UVDPythonExecInterpreter::~UVDPythonExecInterpreter()
+{
+}
+
+uv_err_t UVDPythonExecInterpreter::init()
+{
+	uv_assert_err_ret(UVDPythonInterpreter::init());
+	return UV_ERR_OK;
+}
+
+uv_err_t UVDPythonExecInterpreter::interpret(const UVDInterpreterExpression &exp, const UVDVariableMap &environment, std::string &sRet)
+{
+	/*
+	Do python hack for now
+	[mcmaster@localhost buffer]$ echo 'myvar=4; other=myvar+20; print other' |python -
+	24
+	[mcmaster@localhost uv_udec]$ python -c 'print "test";'
+	test
+	*/
+	static std::string pythonFile;
+	
+	uv_err_t rc = UV_ERR_GENERAL;
+	std::vector<std::string> args;
+	std::string sPythonProgram;
+	int iRet = 0;
+	std::string sErr;
+
+	if( pythonFile.empty() )
+	{
+		uv_assert_err(getTempFile(pythonFile));
+	}
+
+	//Execut an expression
+	//args.push_back("-c");
+	args.push_back(pythonFile);
+
+	uv_assert_err_ret(preparePythonProgram(exp, environment, sPythonProgram));
+
+
+	if( UV_FAILED(writeFile(pythonFile, sPythonProgram)) )
+	{
+		UV_DEBUG(rc);
+		goto error;
+	}	
+	
+	//args.push_back(sPythonProgram);
+	
+	/*
+	Run it!
+	*/
+	uv_assert_err(executeToText("python",
+			args,
+			iRet,
+			&sRet,
+			&sErr));
+
+	if( iRet )
+	{
+		UV_DEBUG(rc);
+		goto error;
+	}
+
+	printf_debug("Raw python result: %s\n", sRet.c_str());
+	if( !sErr.empty() )
+	{
+		printf_debug("Python program:\n%s\n\n", sPythonProgram.c_str());
+		printf_debug("Python error: %s\n", sErr.c_str());
+	}
+		
+	rc = UV_ERR_OK;
+	
+error:
+	UV_DEBUG(deleteFile(pythonFile));
+	return UV_DEBUG(rc);
+}
+
+#endif //USING_PYTHON_EXEC
+
