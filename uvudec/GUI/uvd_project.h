46d3a7f002b1de99cd320d8b0c5381b65c69772b
John McMaster <JohnDMcMaster@gmail.com>
2010-07-14 08:22:28 -0400

    GUI project settings class

---
+++ uvudec/GUI/uvd_project.h
@@ -0,0 +1,15 @@
+/*
+UVNet Universal Decompiler (uvudec)
+Copyright 2008 John McMaster <JohnDMcMaster@gmail.com>
+Licensed under terms of the three clause BSD license, see LICENSE for details
+*/
+
+class UVDProject
+{
+public:
+	UVDProject()
+	~UVDProject();
+
+public:
+	const std::string m_projectName;
+}
