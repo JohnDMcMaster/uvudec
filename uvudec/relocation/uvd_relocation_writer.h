1c55d7d80c684d6c1509f114996ee5c2e056affb
John McMaster <JohnDMcMaster@gmail.com>
2010-09-04 23:32:02 -0400

    UVDRelocationWriter util class

---
+++ uvudec/relocation/uvd_relocation_writer.h
@@ -0,0 +1,40 @@
+/*
+UVNet Universal Decompiler (uvudec)
+Copyright 2010 John McMaster <JohnDMcMaster@gmail.com>
+Licensed under the terms of the LGPL V3 or later, see COPYING for details
+*/
+
+#ifndef UVD_RELOCATION_WRITER_H
+
+#include "uvd_types.h"
+#include "uvd_data.h"
+#include "uvd_relocation.h"
+
+class UVDRelocationWriter
+{
+public:
+	UVDRelocationWriter();
+	~UVDRelocationWriter();
+
+	//Output data should be deleted by callee
+	//It is reconstructed every time and not cached
+	uv_err_t constructBinary(UVDData **data);
+
+	//Phase 1
+	virtual uv_err_t updateForWrite() = 0;
+
+	//Phase 2
+	virtual uv_err_t construct() = 0;
+
+	//Phase 3
+	virtual uv_err_t applyRelocations() = 0;
+
+	uv_err_t addRelocatableData(char *dataIn, uint32_t dataInSize, bool shouldFreeData = false);
+	uv_err_t addRelocatableData(UVDData *data, bool shouldFreeData = false);
+
+public:
+	UVDRelocationManager m_relocationManager;
+};
+
+#endif
+
