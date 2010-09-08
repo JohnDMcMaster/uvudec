170bdf37369fa1db1fab81ea759dd52524a10b8a
John McMaster <JohnDMcMaster@gmail.com>
2010-09-08 09:43:11 -0400

    stack trace space fix and deleted commented out code

--- uvudec/util/uvd_debug.cpp
+++ uvudec/util/uvd_debug.cpp
@@ -275,7 +275,7 @@ void uvd_print_trace(const char *file, int line, const char *function)
 					if( UV_SUCCEEDED(gccCompiler.demangleByABI(functionNameRaw, functionNameDemangled)) )
 					{
 						//Mimic the rest of the stack
-						printf("    %s(%s+%s)[%08x]\n", module.c_str(), functionNameDemangled.c_str(), functionOffsetRaw.c_str(), (int)stack_addrs[i]);					
+						printf("    %s(%s+%s) [%08x]\n", module.c_str(), functionNameDemangled.c_str(), functionOffsetRaw.c_str(), (int)stack_addrs[i]);					
 						continue;
 					}
 				}
@@ -285,59 +285,6 @@ void uvd_print_trace(const char *file, int line, const char *function)
 	}
 	free(stack_strings); // malloc()ed by backtrace_symbols
 	fflush(stdout);
- #if 0
-	const size_t max_depth = 100;
-	size_t stack_depth;
-	void *stack_addrs[max_depth];
-	char **stack_strings;
-
-	stack_depth = backtrace(stack_addrs, max_depth);
-	stack_strings = backtrace_symbols(stack_addrs, stack_depth);
-
-	printf("Call stack from %s @ %s:%d:\n", function, file, line);
-
-	for (size_t i = 1; i < stack_depth; i++) {
-		size_t sz = 200; // just a guess, template names will go much wider
-		char *function = (char *)malloc(sz);
-		char *begin = 0, *end = 0;
-		// find the parentheses and address offset surrounding the mangled name
-		for (char *j = stack_strings[i]; *j; ++j) {
-			if (*j == '(') {
-				begin = j;
-			}
-			else if (*j == '+') {
-				end = j;
-			}
-		}
-		if (begin && end) {
-			*begin++ = ' ';
-			*end = ' ';
-			// found our mangled name, now in [begin, end)
-
-			int status;
-			char *ret = abi::__cxa_demangle(begin, function, &sz, &status);
-			if (ret) {
-				// return value may be a realloc() of the input
-				function = ret;
-			}
-			else {
-				// demangling failed, just pretend it's a C function with no args
-				strncpy(function, begin, sz);
-				strncat(function, "()", sz);
-				function[sz-1] = ' ';
-			}
-			printf("	%s:%s\n", stack_strings[i], function);
-		}
-		else
-		{
-			// didn't find the mangled name, just print the whole line
-			printf("	%s\n", stack_strings[i]);
-		}
-		free(function);
-	}
-	free(stack_strings); // malloc()ed by backtrace_symbols
-	fflush(stdout);
-#endif
 }
 #else
 void uvd_print_trace(const char *file, int line, const char *function)
