43693faa0c93620e6836840717efe78c70adebe3
John McMaster <JohnDMcMaster@gmail.com>
2010-11-26 15:59:30 -0800

    Copyright header standardization

--- uvudec/plugin/uvdasm/interpreter/javascript_spiderape.cpp
+++ uvudec/plugin/uvdasm/interpreter/javascript_spiderape.cpp
@@ -1,7 +1,6 @@
 /*
 UVNet Universal Decompiler (uvudec)
-Copyright 2008 John McMaster
-JohnDMcMaster@gmail.com
+Copyright 2008 John McMaster <JohnDMcMaster@gmail.com>
 Licensed under the terms of the LGPL V3 or later, see COPYING for details
 */
 
--- uvudec/plugin/uvdasm/interpreter/javascript_spidermonkey.cpp
+++ uvudec/plugin/uvdasm/interpreter/javascript_spidermonkey.cpp
@@ -1,6 +1,7 @@
 /*
-Copyright 2008 John McMaster
-JohnDMcMaster@gmail.com
+UVNet Universal Decompiler (uvudec)
+Copyright 2008 John McMaster <JohnDMcMaster@gmail.com>
+Licensed under the terms of the GPL V3 or later, see COPYING for details
 */
 
 #if 0
