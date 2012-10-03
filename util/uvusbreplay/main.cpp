7247aed9fe9343c763b8724bd7f79b86f001df10
John McMaster <JohnDMcMaster@gmail.com>
2012-10-02 22:29:23 -0700

    uvusbreplay fixes/updates

--- util/uvusbreplay/main.cpp
+++ util/uvusbreplay/main.cpp
@@ -1,4 +1,6 @@
 /*
+I write a lot of nice code...this is not bad but not pretty either
+
 Wanted to do this in python but couldn't get python bindings to work even after trying newest version of libpcap
 Expect the Lua bindings are out of date and I don't feel like messing with them
 Python is good for dev, but SIGSEGV in Python is ugly...
@@ -29,6 +31,9 @@ unsigned int g_max_packet = UINT_MAX;
 bool g_error = false;
 bool g_halt_error = true;
 bool g_verbose = false;
+bool g_allow_short = false;
+bool g_custom_call = false;
+bool g_use_defines = false;
 
 #define URB_IS_IN( _urb) (((_urb)->endpoint & USB_DIR_IN) == USB_DIR_IN)
 
@@ -400,11 +405,14 @@ void loop_cb(u_char *args, const struct pcap_pkthdr *header,
 		//std::string device_str = "dev->udev";
 		std::string device_str = "udev";
 		std::string pipe_str;
-		std::string timeout = "500";
-		if (submit.m_ctrl.bRequestType & USB_DIR_IN) {
-			pipe_str = UVDSprintf( "usb_rcvctrlpipe(%s, 0)", device_str.c_str() );
+		std::string timeout;
+		
+		if (g_output_target == OUTPUT_LIBUSB) {
+			pipe_str = "";
+		} else if (submit.m_ctrl.bRequestType & USB_DIR_IN) {
+			pipe_str = UVDSprintf( "usb_rcvctrlpipe(%s, 0), ", device_str.c_str() );
 		} else {
-			pipe_str = UVDSprintf( "usb_sndctrlpipe(%s, 0)", device_str.c_str() );
+			pipe_str = UVDSprintf( "usb_sndctrlpipe(%s, 0), ", device_str.c_str() );
 		}
 	
 		if (keep_packet(submit)) {
@@ -428,17 +436,64 @@ void loop_cb(u_char *args, const struct pcap_pkthdr *header,
 			}
 		}
 		if (keep_packet(submit)) {
-			oprintf("n_rw = usb_control_msg(%s, %s, %s, %s, 0x%04X, 0x%04X, %s, %u, %s);\n",
-					device_str.c_str(), pipe_str.c_str(),
-					//get_request_str(submit.m_ctrl.bRequest).c_str(),
-					get_request_str( submit.m_ctrl.bRequestType, submit.m_ctrl.bRequest ).c_str(),
-					get_request_type_str(submit.m_ctrl.bRequestType).c_str(),
+		    oprintf("n_rw = ");
+		    /*
+            unsigned int dev_control_message(int requesttype, int request,
+	                int value, int index, char *bytes, int size) {
+	                
+            WARNING: request / request type parameters are swapped between kernel and libusb
+            request type is clearly listed first in USB spec and seems more logically first so I'm going to blame kernel on this
+            although maybe libusb came after and was trying for multi OS comatibility right off the bat
+            
+            Anyway, use dev_control_message for finer grained (eg macro) support
+
+            libusb
+            int usb_control_msg(usb_dev_handle *dev, int requesttype, int request, int value, int index, char *bytes, int size, int timeout);
+
+            kernel
+            extern int usb_control_msg(struct usb_device *dev, unsigned int pipe,
+	            __u8 request, __u8 requesttype, __u16 value, __u16 index,
+	            void *data, __u16 size, int timeout);
+		    
+		    Example output:
+		    n_rw = dev_ctrl_msg(0x0B, USB_DIR_IN | USB_TYPE_VENDOR | USB_RECIP_DEVICE, 0xAD16, 0xAD15, buff, 1, 500);
+            */
+		    if (g_custom_call) {
+		        oprintf("dev_ctrl_msg(");
+	        } else {
+		        oprintf("usb_control_msg(%s, ", device_str.c_str());
+	        }
+	        oprintf("%s", pipe_str.c_str());
+		    
+		    std::string bRequestStr = get_request_str( submit.m_ctrl.bRequestType, submit.m_ctrl.bRequest );
+            std::string bRequestTypeStr = "";
+            
+            if (g_output_target == OUTPUT_LIBUSB && !g_use_defines) {
+                bRequestTypeStr = UVDSprintf("0x%02X", submit.m_ctrl.bRequestType);
+            } else {
+                bRequestTypeStr = get_request_type_str(submit.m_ctrl.bRequestType);
+            }
+    		
+    		if (g_output_target == OUTPUT_LIBUSB) {
+    		    oprintf("%s, %s, ", bRequestTypeStr.c_str(), bRequestStr.c_str());
+    		} else {
+    		    oprintf("%s, %s, ", bRequestStr.c_str(), bRequestTypeStr.c_str());
+    		}
+    		
+    		if (g_custom_call) {
+    		    timeout = "";
+    		} else {
+    		    timeout = ", 500";
+    		}
+    		
+			oprintf("0x%04X, 0x%04X, %s, %u%s);\n",
 					submit.m_ctrl.wValue, submit.m_ctrl.wIndex,
 					data_str.c_str(), data_size,
 					timeout.c_str() );
 		}
 		if (submit.m_ctrl.bRequestType & USB_DIR_IN) {
-			unsigned int payload_sz = submit.m_ctrl.wLength;
+			unsigned int max_payload_sz = submit.m_ctrl.wLength;
+			unsigned int payload_sz = 0;
 			
 			//Take off the unknown struct
 			if (remaining_bytes < sizeof(control_rx_t)) {
@@ -452,35 +507,66 @@ void loop_cb(u_char *args, const struct pcap_pkthdr *header,
 			remaining_bytes -= sizeof(control_rx_t);
 			
 			//Verify we actually have enough
-			if (remaining_bytes != payload_sz) {
-				printf("expected remaining bytes %u to be the requested length %u\n",
-						remaining_bytes, payload_sz );
-				if (g_halt_error) {
-					exit(1);
-				}
-				return;
-			}
+			if (remaining_bytes == max_payload_sz) {
+			    //Exact match
+			    payload_sz = max_payload_sz;			
+			} else if (g_allow_short) {
+		        printf("//WARNING: shrinking response, max %u but got %u\n", max_payload_sz, remaining_bytes);
+		        payload_sz = remaining_bytes;
+	        } else {
+			    printf("expected remaining bytes %u to be the requested length %u\n",
+					    remaining_bytes, payload_sz );
+			    if (g_halt_error) {
+				    exit(1);
+			    }
+			    return;
+		    }
 			std::string byte_str;
 			uint8_t *payload = NULL;
 			payload = dat_cur;
 			std::string pad = "";
 			for (unsigned int i = 0; i < payload_sz; ++i) {
+				if (i % 16 == 0) {
+				    pad = "";
+				    if (i != 0) {
+				        byte_str += ",\n        ";
+				    }
+				}
 				byte_str += pad;
 				byte_str += UVDSprintf("0x%02X", payload[i]);
 				pad = ", ";
 			}
 			if (keep_packet(submit)) {
-				oprintf("if (validate_read((char[]){%s}, %u, buff, n_rw, \"packet %u/%u\") < 0)\n",
+			    if (!g_custom_call) {
+    				oprintf("if (");
+			    }
+				oprintf("validate_read((char[]){%s}, %u, buff, n_rw, \"packet %u/%u\")",
 						byte_str.c_str(), payload_sz, 
 						submit.packet_number, g_cur_packet );
-				oprintf("\treturn 1;\n");
+				if (!g_custom_call) {
+				    oprintf(" < 0)\n");
+				    oprintf("\treturn 1;\n");
+			    } else {
+				    oprintf(";\n");
+			    }
 			}
 			remaining_bytes -= submit.m_ctrl.wLength;
 		} else {
 			if (keep_packet(submit)) {
-				oprintf("if (validate_write(%u, n_rw, \"packet %u/%u\") < 0)\n",
+				/*
+				is there really anything to validate on a write?
+				if (!g_custom_call) {
+				    oprintf("if (");
+				}
+				oprintf("validate_write(%u, n_rw, \"packet %u/%u\")",
 						data_size, submit.packet_number, g_cur_packet );
-				oprintf("\treturn 1;\n");
+				if (g_custom_call) {
+				    oprintf(";\n");
+				} else {
+				    oprintf(" < 0)\n");
+				    oprintf("\treturn 1;\n");
+			    }
+			    */
 			}
 		}
 	} else if (urb->transfer_type == URB_BULK) {
@@ -525,7 +611,7 @@ int main(int argc, char *argv[])
 	opterr = 0;
 
 	while (true) {
-		int c = getopt(argc, argv, "r:klh?v");
+		int c = getopt(argc, argv, "r:klh?vsf");
 		
 		if (c == -1) {
 			break;
@@ -550,6 +636,14 @@ int main(int argc, char *argv[])
 			case 'l':
 				g_output_target = OUTPUT_LIBUSB;
 				break;
+			
+			case 's':
+				g_allow_short = true;
+				break;
+			
+			case 'f':
+			    g_custom_call = true;
+			    break;
 			
 			case 'h':
 			case '?':
@@ -583,7 +677,6 @@ int main(int argc, char *argv[])
 		++raws;
 	}
 	
-
 	oprintf("/*\n");
 	oprintf("Generated by uvusbreplay %s\n", VERSION_STR);
 	oprintf("uvusbreplay copyright 2011 John McMaster <JohnDMcMaster@gmail.com>\n");
@@ -593,6 +686,21 @@ int main(int argc, char *argv[])
 	oprintf("*/\n");
 	oprintf("int n_rw = 0;\n");
 	oprintf("char buff[64];\n");
+	
+	if (g_output_target == OUTPUT_LIBUSB && g_use_defines) {
+	    //Libusb expects users to hard code these into address I guess
+        oprintf("//Directions\n");
+        oprintf("//to device\n");
+        oprintf("const int USB_DIR_OUT = 0;\n");
+        oprintf("//to host\n");
+        oprintf("const int USB_DIR_IN = 0x80;\n");
+        oprintf("const int USB_TYPE_MASK = (0x03 << 5);\n");
+        oprintf("const int USB_TYPE_STANDARD = (0x00 << 5);\n");
+        oprintf("const int USB_TYPE_CLASS = (0x01 << 5);\n");
+        oprintf("const int USB_TYPE_VENDOR = (0x02 << 5);\n");
+        oprintf("const int USB_TYPE_RESERVED = (0x03 << 5);\n");
+	}
+    oprintf("\n");
 	
 	dbg("parsing from range %u to %u\n", g_min_packet, g_max_packet);
 	p = pcap_open_offline(fileName.c_str(), errbuf);
